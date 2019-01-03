580e3f4d48e9f1bc07d96d3343b3623ab168b37b
Lolsogod <46161640+Lolsogod@users.noreply.github.com>
2019-01-03 13:50:20 +0400

    Aspect ratio check

--- public/tooltip.js
+++ public/tooltip.js
@@ -2,3 +2,11 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+const check = (img) => {
+  let w = img.clientWidth;
+  let h = img.clientHeight;
+  if(h/w > 8 || w/h > 8){
+      img.src = 'https://www.ricoh-europe.com/img/error.jpeg';
+  }
+};
