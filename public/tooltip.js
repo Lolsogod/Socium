4b64c8b1dacbd44d646870de77f8376157da7fc5
Lolsogod <46161640+Lolsogod@users.noreply.github.com>
2019-01-03 14:34:31 +0400

    fixes

--- public/tooltip.js
+++ public/tooltip.js
@@ -6,7 +6,7 @@ $(function () {
 const check = (img) => {
   let w = img.clientWidth;
   let h = img.clientHeight;
-  if(h/w > 12 || w/h > 7 || img.src.match(/\.(jpeg|jpg|gif|png)$/) == null){
+  if(h/w > 19 || w/h > 7 || img.src.match(/\.(jpeg|jpg|gif|png)$/) == null){
       img.src = 'https://www.ricoh-europe.com/img/error.jpeg';
   }
 };
